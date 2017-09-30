e14d2898aa2702ff71077736ac5ed14bdea60ca7
thebiteffect <theblindbandet@users.noreply.github.com>
2017-09-30 15:19:30 -0400

    Added else statement to the if statement for testing... And it works!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanicTestRev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanicTestRev.java
@@ -4,17 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "PanicDEBUG: Rev Test", group = "PanicDEBUG")
+@TeleOp(name = "PanicDEBUG: Rev Test")
 public class PanicTestRev extends OpMode {
 
     DcMotor motorLeft;
     DcMotor motorRight;
 
+
     @Override
     public void init() {
         motorLeft = hardwareMap.dcMotor.get("Left Motor");
         motorRight = hardwareMap.dcMotor.get("Right Motor");
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorLeft.setDirection(DcMotor.Direction.FORWARD);
         motorRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
@@ -26,8 +27,11 @@ public class PanicTestRev extends OpMode {
         } else if (gamepad1.x) {
             motorRight.setPower(-1);
             motorLeft.setPower(-1);
+        } else {
+            motorRight.setPower(0);
+            motorLeft.setPower(0);
         }
-
+        Thread.yield();
     }
 
     @Override
