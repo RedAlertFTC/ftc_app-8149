cb5ed8e41fb1c1b149d3421af3562e1606c394b3
Benji Berners <berneben000@students.centergrove.k12.in.us>
2018-10-24 15:52:51 -0400

    First (almost complete) draft of CollectionSystem
    I don't yet  know what the default speed of the motor should be, and there are
    probably some other things that I am doing horribly wrong.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollectionSystem.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/* This class, I guess, does not include the extension arm, I guess.  This is
+   just the part that actually collects and uncollects stuff.  Maybe someone'll
+   decide that should be done differently, though?  But, for now, this is just
+   that part.
+   FIXME: It's not done.
+ */
+public class CollectionSystem {
+
+    /* Specify for now that this is the one that actually collects, in case we
+    control multiple motors with this class. */
+    /* Is this a DC Motor? Probably. */
+    public DcMotor CollectionMotor;
+    public String CollectionMotorName;
+
+    /* Why make it double?  I don't know.  I felt like it. */
+    /* This variable is not really used; it jus follows the speed of the motor
+     * in case someone wants to look at it.  Is this necessary?
+     */
+    public double CollectionSpeed;
+
+    /* FIXME: initialize this. It should be positive.
+    * This is a constant; it would be a macro if Java had a preprocessor. */
+    public double UsualSpeed;
+
+    public CollectionSystem(String MotorName) {
+        CollectionMotorName = MotorName;
+    }
+
+    public CollectionSystem() {
+        new CollectionSystem("motorCollection");
+        /* Should InitMotor be called here? */
+    }
+
+
+    /* I don't know if I'm doing this right.  I'm basically doing whatever was
+     * in utils/MecanumDrive.java.
+     */
+    public void InitMotor(HardwareMap HardwareMap) {
+        /* I think that thebiteffect did this part wrong; they didn't actually
+        use the strings they got from the constructor, assuming the defaults.
+         */
+        CollectionMotor = HardwareMap.dcMotor.get(CollectionMotorName);
+
+        CollectionMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        CollectionMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        CollectionSpeed = 0;
+    }
+
+    public void SetSpeed(double NewSpeed) {
+        CollectionMotor.setPower(NewSpeed);
+        CollectionSpeed = NewSpeed;
+    }
+
+    public void Collect() {
+        SetSpeed(UsualSpeed);
+    }
+
+    public void Eject() {
+        SetSpeed(-UsualSpeed);
+    }
+
+    public void Stop() {
+        SetSpeed(0);
+    }
+}
