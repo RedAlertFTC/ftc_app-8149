d5ba941a6a9380c4373917453203574765b4bd90
Benji Berners <berneben000@students.centergrove.k12.in.us>
2018-11-12 20:04:20 -0500

    Stub class for collection system extension arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollectionSystemArm.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+/* FIXME: Fix me please! */
+
+public class CollectionSystemArm {
+    /* TODO: like everything
+    so there'll be some motors or something here.
+     */
+
+    public CollectionSystemArm(/*TODO*/) {
+        /*FIXME*/
+    }
+
+    public void initMotors() {
+        /*FIXME*/
+    }
+
+    public void setVerticalPosition(double position) {
+        /*FIXME*/
+    }
+
+    public void setExtension(double position) {
+        /*FIXME*/
+    }
+}
