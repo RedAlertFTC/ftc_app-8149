7cdf459ab8cd6fea1c59e24f748fe6f883c32ae8
theblindbandet <theblindbandet@users.noreply.github.com>
2018-12-03 18:11:14 -0500

    Add sampling code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanicAutonomousBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PanicAutonomousBase.java
@@ -1,28 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.utils.MecanumDrive;
 
 public class PanicAutonomousBase extends LinearOpMode {
+    GoldAlignDetector detector;
     MecanumDrive drive = new MecanumDrive();
+    LiftSystem lift = new LiftSystem();
+    CollectionSystem collection = new CollectionSystem();
     TeamColor currentTeam;
     ProgramType currentProgramType;
-    // OpenGLMatrix whereWeAre = new OpenGLMatrix(), whereWeNeedToGo = new OpenGLMatrix();
     /* This might change, as we might combine the collection and lift arms. */
-    LiftSystem lift = new LiftSystem();
+    // OpenGLMatrix whereWeAre = new OpenGLMatrix(), whereWeNeedToGo = new OpenGLMatrix();
     long FIXME = 0; // for error free builds!
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // Set up detector
+        detector = new GoldAlignDetector(); // Create detector-
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+
+        // Optional tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+        detector.enable(); // Start the detector!
+
         drive.InitMotors(hardwareMap); // Init drive train
         lift.initMotor(hardwareMap);
+
+        drive.setMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
         waitForStart(); // Wait for the start
 
         /* Landing */
         lift.extend();
         sleep(6750);
-        drive.update(0, 0.25, 0);
+        drive.updateTarget(0, 0, 0);
         sleep(100); /* FIXME: probably way off */
         drive.stop();
 
@@ -50,7 +77,7 @@ public class PanicAutonomousBase extends LinearOpMode {
 
         /* Claim: drop the marker. */
 
-        /* Now sampling.  Ideally we vould be able to pass flags at invocation
+        /* Now sampling.  Ideally we would be able to pass flags at invocation
          * time to specify what sampling we are doing depending on what the
          * other team does, but is seems that Java runtime does not support
          * that in any convenient way. */
@@ -58,16 +85,33 @@ public class PanicAutonomousBase extends LinearOpMode {
         /* Sampling 1: drive to the first sampling place. */
 
         /* Sampling 1: go until we find the gold mineral. */
+        do {
+            drive.update(0, -0.3, 0);
+        } while (!detector.getAligned());
 
+        drive.stop();
         /* Sampling 1: do something to the mineral.  Will we pick it up or just
          * bump it? */
-
-        /* Sampling 2: drive to the second sampling place. */
+        /* We will... */
+        drive.update(1, 0, 0);
+        sleep(250);
+        drive.stop();
+        sleep(500);
+        drive.update(-0.5, 0, 0);
+        sleep(500);
+        /* Sampling 2: drive to the second sampling place. IF we are  */
 
         /* Sampling 2: go until we find the gold mineral */
-
+        do {
+            drive.update(0, -0.3, 0);
+        } while (!detector.getAligned());
         /* Sampling 2: do something to the mineral. */
-
+        drive.update(1, 0, 0);
+        sleep(250);
+        drive.stop();
+        sleep(500);
+        drive.update(-0.5, 0, 0);
+        sleep(500);
         /* If we picked up a mineral, maybe we could put it in the depot or
          * the cargo hold. */
 
